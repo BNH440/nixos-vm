91b0f94c1ea3ce5786d60847e0868cc7f3872f8f
Blake Haug <blake@blakehaug.com>
2025-12-24 21:50:18 -0800

    Add 'starship' prompt

--- flake.nix
+++ flake.nix
@@ -19,7 +19,7 @@
         {
           home-manager.useGlobalPkgs = true;
           home-manager.useUserPackages = true;
-          home-manager.users.blakeh = ./modules/home.nix;
+          home-manager.users.blakeh = ./modules/home/home.nix;
         }
       ];
     };
